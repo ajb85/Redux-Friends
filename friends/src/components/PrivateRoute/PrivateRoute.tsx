531eea055dfeddc273968f97ca6cb2477c599250
ajb85 <ajbrush85@gmail.com>
2019-03-07 18:33:13 -0500

    PrivateRoute added

--- friends/src/components/PrivateRoute/PrivateRoute.tsx
+++ friends/src/components/PrivateRoute/PrivateRoute.tsx
@@ -6,6 +6,19 @@ interface Props {
 }
 // Another way to say "Leave me alone, TypeScript, I don't know what I'm doing yet"
 
-const PrivateRoute = ({ component: Component, ...rest }: Props) => {};
+const PrivateRoute = ({ component: Component, ...rest }: Props) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
